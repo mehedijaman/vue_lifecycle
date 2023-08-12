506630e82a4825ec85417e39b87c1c3c5d80df14
Mehedi Jaman <mail4mjaman@gmail.com>
2023-08-12 22:27:43 +0600

    Carousel Done

--- src/components/CarouselSlider.vue
+++ src/components/CarouselSlider.vue
@@ -1,11 +1,24 @@
 <script setup>
-import { ref, reactive, onMounted, nextTick } from 'vue'
+import { ref, reactive, onMounted, onBeforeUnmount, nextTick } from 'vue'
 
-const items = [
-    'https://images.unsplash.com/photo-1682687982046-e5e46906bc6e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
-    'https://images.unsplash.com/photo-1682686581498-5e85c7228119?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
-    'https://images.unsplash.com/photo-1682687982167-d7fb3ed8541d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80'
-]
+const items = ref([
+    {
+        title:'Memory feeds imagination.',
+        url:'https://images.unsplash.com/photo-1682687982046-e5e46906bc6e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
+    },
+    {
+        title:'Gardens and flowers have a way of bringing people together, drawing them from their homes.',
+        url:'https://images.unsplash.com/photo-1682686581498-5e85c7228119?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
+    },
+    {
+        title:'While you are not able to serve men, how can you serve spirits [of the dead]?...While you do not know life, how can you know about death?',
+        url:'https://images.unsplash.com/photo-1682687982167-d7fb3ed8541d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80'
+    },
+    {
+        title:'In times like these, it helps to recall that there have always been times like these.',
+        url:'https://images.unsplash.com/photo-1682686581030-7fa4ea2b96c3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
+    },
+])
 
 let carousel = null
 
@@ -13,10 +26,17 @@ onMounted(() =>{
     carousel = new Flickity( '#carousel', {});
 })
 
-let newItem = ref('')
+onBeforeUnmount(() => {
+    carousel.destroy()
+})
+
+let newItem = reactive({
+    title:'',
+    url:''
+})
 
 function updateCarousel(){
-    items.push(newItem.value)
+    items.value.push(newItem)
     console.log(items)
     carousel.destroy()
 
@@ -28,28 +48,34 @@ function updateCarousel(){
 </script>
 <template>
     <form>
-        <div class="grid gap-6 mb-6 md:grid-cols-2">
+        <div class="grid gap-6 mb-6 md:grid-cols-3">
         <div>
-            <input v-model="newItem" type="text" id="color-name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required placeholder="Image URL">
+            <input v-model="newItem.url" type="text" id="color-name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required placeholder="Image URL">
         </div>
+        <div>
+            <input v-model="newItem.title" type="text" id="color-name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required placeholder="Title">
+        </div>
+
         <div>
             <button @click.prevent="updateCarousel()" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to Carousel</button>
         </div>        
         </div>
     </form>
     <div class="mx-auto items" id="carousel">
-        <div class="item" :style="`background-image:url(${item})`" v-for="(item, index) in items" :key="index">{{ index+1 }}</div>
+        <div class="item" :style="`background-image:url(${item.url})`" v-for="(item, index) in items" :key="index">
+            <p class="pl-12 pr-12 pt-80 text-white font-bold ">{{ item.title }}</p>
+        </div>
     </div>    
 </template>
 
 <style scoped>
 .items{
-    width: 800px;
+    width: 1000px;
     height:400px;
 }
 
 .item{
-    width: 800px;
+    width: 1000px;
     height: 400px;
     background-color: gray;
     background-size: cover;
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,6 +1,7 @@
 <script></script>
 <template>
     <div class="p-1">
-        <h1 class="text-xl text-center">Homepage</h1>
+        <h1 class="text-xl text-center">Hello & Welcome</h1>
+        <h1 class="text-xl text-center">Please navigate to the Carousel and Chart Pages from the right sidebar</h1>
     </div>
 </template>
