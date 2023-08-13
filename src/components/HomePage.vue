35734ed867e025f1bb64012fecf5e2bfbf3d57a1
Mehedi Jaman <mail4mjaman@gmail.com>
2023-08-13 09:20:02 +0600

    Flowbite CSS and JS included

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,6 +2,6 @@
 <template>
     <div class="p-1">
         <h1 class="text-xl text-center">Hello & Welcome</h1>
-        <h1 class="text-xl text-center">Please navigate to the Carousel and Chart Pages from the right sidebar</h1>
+        <h1 class="text-xl text-center">Please navigate to the Carousel and Chart Pages from the left sidebar</h1>
     </div>
 </template>
